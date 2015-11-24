908f7cb71e67121c91b1529c4fc1596a8ab9b3c0
James Woglom <j@wogloms.net>
2015-11-23 20:55:42 -0500

    urls regex, no pyc

--- tjdests/urls.py
+++ tjdests/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
     url(r'^logout$', views.logout_view, name='logout'),
     url(r'^accounts/login/$', views.login_view),
     url(r'^register$', views.register_view, name='register'),
-    url(r'^verify/(?P<verify_key>.*)?$', views.verify_view, name='verify'),
+    url(r'^verify/(?P<verify_key>[\w-]+)?$', views.verify_view, name='verify'),
 
     url(r'^students$', views.students_view, name='students'),
     url(r'^colleges$', views.colleges_view, name='colleges'),
